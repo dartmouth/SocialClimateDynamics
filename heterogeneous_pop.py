e94f81e1046bf47f7b45da3b4edd8c780018c80b
Luke Wisniewski <lukewisniewski04@gmail.com>
2025-04-28 13:30:02 -0400

    Altering Parameters Manually

--- heterogeneous_pop.py
+++ heterogeneous_pop.py
@@ -10,30 +10,30 @@ from dde_figs import uncoupled_model
 
 gamma = 0.2 # Disturbance rate
 fmax = 5 # Maximum warming cost, between 4 and 6
-alpha_r = 0.45 # Cost of mitigation for rich group, between 0.45 and 0.55
-delta = 2 # Strength of social norms, between 0.5 and 1.5
+alpha_r = 0.5 # Cost of mitigation for rich group, between 0.45 and 0.55
+delta = 1 # Strength of social norms, between 0.5 and 1.5
 kappa = 0.05 # Social learning rate, between 0.02 and 0.2
-alpha_p0 = 0.9 # Cost of mitigation for poor group, between 0.9 and 1.1
+alpha_p0 = 1 # Cost of mitigation for poor group, between 0.9 and 1.1
 xr0 = 0.9 # Initial mitigation rate in the rich subpopulation
 xp0 = 0.9 # Initial mitigation rate in the poor subpopulation
-ir0 = 4.5 # Initial income of rich group, between 4.5 and 5.5
+ir0 = 5 # Initial income of rich group, between 4.5 and 5.5
 ip0 = 3.85 # Initial income of poor group, between 3.15 and 3.85
-rho = 0.25 # Size of rich group relative to poor group, between 0 and 0.5
+rho = 0.5 # Proportion of total population in the rich group, between 0 and 0.5
 cr = 0.4 # Maximum of income cost function for rich subpopulation, between 0.36 and 0.44
 cp = 0.85 # Maximum of income cost function for poor subpopulation, between 0.765 and 0.935
 kr = 1.1 # Nonlinearity of income impact function for rich subpopulation, between 0.9 and 1.1
 kp = 1.35 # Nonlinearity of income impact function for poor subpopulation, between 1.35 and 1.65
-h = 0 # Homophily parameter, between 0 and 1
-d = 5 # Maximum cost of dissatisfaction, between 0 and 10
+h = 1 # Homophily parameter, between 0 and 1
+d = 0 # Maximum cost of dissatisfaction, between 0 and 10
 w = 3 # Nonlinearity of warming cost, between 2.7 and 3.3
 omega = 3 # Nonlinearity of dissatisfaction cost, between 2.7 and 3.3
 dc = 1.5 # Critical value of dissatisfaction cost, between 1.35 and 1.65
-s = 1.5 # Effectiveness of mitigative action scaling, >= 1
+s = 1 # Effectiveness of mitigative action scaling, >= 1
 
 time_delay = 10
 tspan = np.linspace(time_delay, 200, 10000)
 
-def heterogeneous_pop_model(Y, t, delay, Tv, Tc):
+def heterogeneous_pop_model(Y, t, delay, Tv, Tc, fmax, alpha_r, delta, kappa, alpha_p0, xr0, xp0, ir0, ip0, rho, cr, cp, kr, kp, h, d, w, omega, dc, s):
     v, xp, xr = Y(t)
     v_lag, xp_lag, xr_lag = Y(t-delay)
     gr = 1- (cr/(1+np.exp(-1*kr*(Tv-5*(1-v)-Tc))))
@@ -81,7 +81,7 @@ if __name__ == "__main__":
     tv_vals = np.linspace(22, 35, 5)
     tc_vals = np.linspace(1, 3, 5)
 
-    test = ddeint(heterogeneous_pop_model, doub_history_function(0.1, 25, xr0, xp0), tspan, fargs=(time_delay, 25, 1.5))
+    test = ddeint(heterogeneous_pop_model, doub_history_function(0.1, 31.5, xr0, xp0), tspan, fargs=(time_delay, 31.5, 2, fmax, alpha_r, delta, kappa, alpha_p0, xr0, xp0, ir0, ip0, rho, cr, cp, kr, kp, h, d, w, omega, dc, s))
     plt.figure(figsize=(10, 6))
     plt.plot(tspan, test[:, 0], label="Vegetation")
     plt.plot(tspan, test[:, 1], label="Mitigation: Rich")
